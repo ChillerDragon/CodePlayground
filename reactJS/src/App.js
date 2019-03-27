f1b483745e66477d2c707fe602c4ba06666f848c
ChillerDragon <ChillerDragon@gmail.com>
2019-03-27 22:03:52 +0100

    Add reactJS

---
+++ reactJS/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Todos from './components/Todos.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ reactJS/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+    return (
+      <div>
+        tofodos
+      </div>
+    );
+  }
+}
+
+export default Todos;
